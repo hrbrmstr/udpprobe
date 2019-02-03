769fc6a015ceb94f9ec1f0e6d60df9916dd978ad
hrbrmstr <bob@rud.is>
2019-02-03 14:12:01 -0500

    R package repo initialization complete

---
+++ R/udpprobe-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/udpprobe>
+#' - BugReports: <https://gitlab.com/hrbrmstr/udpprobe/issues>
+#' 
+#' @md
+#' @name udpprobe
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
